22501022a68ef2220927b3c252dec7316527f75a
BederCC <beder1810@gmail.com>
2025-04-29 23:15:48 -0500

    subiendo archivos a favoritos

--- consumo-api/main.js
+++ consumo-api/main.js
@@ -1,5 +1,7 @@
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk";
-const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?api_key=live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk";
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites";
+const API_URL_DELETE_FAVORITES = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
+const API_URL_UPLOAD = "https://api.thecatapi.com/v1/images/upload";
 
 const spanError = document.getElementById("error");
 
@@ -29,7 +31,12 @@ async function reloadRandomMichis() {
 }
 
 async function loadFavouriteMichis() {
-    const res = await fetch(API_URL_FAVORITES);
+    const res = await fetch(API_URL_FAVORITES,{
+        method: "GET",
+        headers: {
+            'X-API-KEY': 'live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk',
+        }
+    });
     const data = await res.json();
     console.log('favorites');
     console.log(data);
@@ -37,6 +44,14 @@ async function loadFavouriteMichis() {
     if (res.status !== 200) {
         spanError.innerHTML = " Hubo un error " + res.status + " " + data.message;
     } else  {
+        const section = document.getElementById('favoriteMichis');
+        section.innerHTML = ""; // Clear previous favorites
+
+        const h2 = document.createElement('h2');
+        const h2Txt = document.createTextNode('Michis Favoritos');
+        h2.appendChild(h2Txt);
+        section.appendChild(h2);
+
         data.forEach(element => {
             const section = document.getElementById('favoriteMichis');
             const article = document.createElement('article');
@@ -47,6 +62,7 @@ async function loadFavouriteMichis() {
             img.src = element.image.url;
             img.width = 200;
             button.appendChild(btnTxt);
+            button.onclick = () => deleteFavouriteMichi(element.id);
             article.appendChild(img);
             article.appendChild(button);
             section.appendChild(article);
@@ -60,6 +76,7 @@ async function addFavouriteMichi(id) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            'X-API-KEY': 'live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk',
         },
         body: JSON.stringify({
             image_id: id
@@ -72,6 +89,55 @@ async function addFavouriteMichi(id) {
 
     if (res.status !== 200) {
         spanError.innerHTML = " Hubo un error " + res.status + " " + data.message;
+    } else {
+        console.log('Michi guardado');
+        loadFavouriteMichis();
+    }
+}
+
+async function deleteFavouriteMichi(id) {
+    const res = await fetch(API_URL_DELETE_FAVORITES(id), {
+        method: "DELETE"
+        , headers: {
+            "Content-Type": "application/json",
+            'X-API-KEY': 'live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk',
+        }
+    });
+    const data = await res.json();
+    console.log('delete');
+    console.log(res);
+    
+
+    if (res.status !== 200) {
+        spanError.innerHTML = " Hubo un error " + res.status + " " + data.message;
+    } else {
+        console.log('Michi eliminado');
+        loadFavouriteMichis();
+    }
+}
+// aca nos quedamos en el video min 10:50
+async function uploadMichiPhoto() {
+    const form = document.getElementById('uploadingForm');
+    const formData = new FormData(form);
+    console.log(formData.get('file'));
+    const res = await fetch(API_URL_UPLOAD, {
+        method: "POST",
+        headers: {
+            // "Content-Type": "multipart/form-data",
+            'X-API-KEY': 'live_QTdpa5ZGugrYCve6ZWEyWUeFKKf2j42gvD7NzWSwFhn26F41hm5mMhZFX5q2aiDk',
+        },
+        body: formData
+    });
+    const data = await res.json();
+    console.log('upload');
+    console.log(res);
+    
+
+    if (res.status !== 201) {
+        spanError.innerHTML = " Hubo un error " + res.status + " " + data.message;
+    } else {
+        console.log('Michi subido');
+        addFavouriteMichi(data.id);
     }
 }
 
